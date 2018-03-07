81e64bdd6b65cbb9beb2cda7dab55a5e3c89a010
Awei <13650928419@163.com>
2018-03-07 10:52:10 +0800

    v1.3 commit

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 3000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
